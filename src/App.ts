0bc716c563465370defa6c5d98dc5b1b322a8390
LHI0915 <gowns1998@gmail.com>
2023-05-31 13:33:52 +0900

    Fix : import path 오류 해결

--- src/App.ts
+++ src/App.ts
@@ -17,10 +17,10 @@ mongoose.connect(DB_URL, { dbName: 'maum' });
 const db = mongoose.connection;
 
 db.on('connected', () =>
-  console.log('정상적으로 MongoDB 서버에 연결되었습니다.  ' + DB_URL),
+  console.log('정상적으로 MongoDB 서버에 연결되었습니다.  ' + DB_URL)
 );
 db.on('error', (error) =>
-  console.error('\nMongoDB 연결에 실패하였습니다...\n' + DB_URL + '\n' + error),
+  console.error('\nMongoDB 연결에 실패하였습니다...\n' + DB_URL + '\n' + error)
 );
 
 export { app };
