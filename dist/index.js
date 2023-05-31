7ca1c720a99f039395e4b97b5f9cf7d08f87c217
Takkyou <takelucky777@naver.com>
2023-05-31 11:51:09 +0900

    Feat

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-const port = 5001;
-app.get('/', (req, res) => {
-    res.send('Typescript + Node.js + Express Server');
-});
-app.listen(port, () => {
-    console.log(`[server]: Server is running at <https://localhost>:${port}`);
-});
--- src/db/schemas/Review.ts
+++ src/db/schemas/Review.ts
@@ -1,21 +1,17 @@
-import {
-  modelOptions,
-  prop,
-  getModelForClass,
-  Ref,
-} from "@typegoose/typegoose";
+import { modelOptions, prop } from "@typegoose/typegoose";
 
 @modelOptions({ schemaOptions: { timestamps: true } })
-export class Review {
+class Review {
   public _id!: string;
   @prop({ required: true })
   public title!: string;
   @prop({ required: true })
   public content!: string;
-  @prop()
-  public images!: string[];
+
   //   @prop({ ref: Volunteer })
   //   public volunteer_id!: Ref<Volunteer>;
   //   @prop({ ref: User })
   //   public user_id!: Ref<User>;
 }
+
+export { Review };
