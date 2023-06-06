c8c67afdd1ae9fa1ac038b42ff6fc4f6819b0c05
LHI0915 <gowns1998@gmail.com>
2023-06-06 14:10:59 +0900

    Feat : volunteer, comment 부분 loginRequired 설정

--- src/routers/volunteerRouter.ts
+++ src/routers/volunteerRouter.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { VolunteerController } from '../controllers/volunteerController.js';
+import { loginRequired } from '../middlewares/loginRequied.js';
 
 const volunteerRouter = Router();
 const volunteerController = new VolunteerController();
 
-volunteerRouter.post('/volunteers', volunteerController.postVolunteer);
+volunteerRouter.post(
+  '/volunteers',
+  loginRequired,
+  volunteerController.postVolunteer
+);
 
 // 쿼리스트링 : req.query.keyword 형태로 가져옴
 volunteerRouter.get(
@@ -20,17 +25,14 @@ volunteerRouter.get(
 );
 
 volunteerRouter.get(
-  '/volunteers/:userId/application',
-  volunteerController.getApplicationVolunteer
-);
-
-volunteerRouter.get(
   '/volunteers/:userId/registeration',
+  loginRequired,
   volunteerController.getRegisterationVolunteer
 );
 
 volunteerRouter.patch(
   '/volunteers/:volunteerId',
+  loginRequired,
   volunteerController.patchVolunteer
 );
 
