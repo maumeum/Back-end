aad8a14a4028af0dd8bbc17f1d051e75c67cc473
Hanna <foureaf@gmail.com>
2023-06-07 11:11:52 +0900

    Conflict : MergeConflit 해결

--- src/routers/postCommentRouter.ts
+++ src/routers/postCommentRouter.ts
@@ -6,65 +6,30 @@ const postCommentRouter = Router();
 postCommentRouter.post(
   '/postComments',
   loginRequired,
-<<<<<<< HEAD
-<<<<<<< HEAD
   PostCommentController.postComment,
 );
 
-postCommentRouter.post(
-  '/postComments',
-  loginRequired,
-  PostCommentController.postComment,
-=======
-  PostCommentController.postComment
->>>>>>> e357cd205464861b900a4e9d6ae15fc22ecd46bf
-);
-
-=======
-  PostCommentController.postComment,
-);
-
->>>>>>> 50549970b4443470e23f62e76de10a21c5683226
 postCommentRouter.get(
   '/postComments/users',
   loginRequired,
-  PostCommentController.getPostByComment
+  PostCommentController.getPostByComment,
 );
 
 postCommentRouter.get(
   '/postComments/:postId',
-  PostCommentController.getComment
+  PostCommentController.getComment,
 );
 
 postCommentRouter.patch(
   '/postComments/:postCommentId',
   loginRequired,
-<<<<<<< HEAD
-<<<<<<< HEAD
   PostCommentController.patchComment,
-=======
-  PostCommentController.patchComment
->>>>>>> e357cd205464861b900a4e9d6ae15fc22ecd46bf
-=======
-  PostCommentController.patchComment
->>>>>>> 50549970b4443470e23f62e76de10a21c5683226
 );
 
 postCommentRouter.delete(
   '/postComments/:postCommentId',
-<<<<<<< HEAD
-<<<<<<< HEAD
   loginRequired,
   PostCommentController.deleteComment,
-=======
-
-  loginRequired,
-  PostCommentController.deleteComment
->>>>>>> e357cd205464861b900a4e9d6ae15fc22ecd46bf
-=======
-  loginRequired,
-  PostCommentController.deleteComment,
->>>>>>> 50549970b4443470e23f62e76de10a21c5683226
 );
 
 export { postCommentRouter };
