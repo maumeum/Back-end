8895ac789fb02cbf2cb6b23d67a09a9249f8f91b
Takkyou <takelucky777@naver.com>
2023-06-08 15:53:21 +0900

    Fix: 게시물 확인

--- src/routers/communityRouter.ts
+++ src/routers/communityRouter.ts
@@ -35,7 +35,7 @@ communityRouter.get(
 );
 
 //특정 게시물 조회 // 완료
-communityRouter.get("/community/:id", communityController.getPost);
+communityRouter.get("/community/:postid", communityController.getPost);
 
 //특정 게시물 수정
 communityRouter.patch(
@@ -44,6 +44,9 @@ communityRouter.patch(
   loginRequired,
   communityController.patchPost
 );
+
+//게시물 맞는지 확인
+communityRouter.get("/community/check/:id", communityController.checkUser);
 
 //특정 게시물 삭제 //완료
 communityRouter.delete(
