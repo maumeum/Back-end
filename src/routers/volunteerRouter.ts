d4801ebea69107ab27ef092895ee9c6324c84675
LHI0915 <gowns1998@gmail.com>
2023-06-09 18:44:05 +0900

    Feat : volunteer 추가 및 수정 시  multer 기능 추가

--- src/routers/volunteerRouter.ts
+++ src/routers/volunteerRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { VolunteerController } from '../controllers/index.js';
 import { loginRequired } from '../middlewares/loginRequired.js';
 import { makeInstance } from '../utils/makeInstance.js';
+import { imagesUploader } from '../utils/multer.js';
 
 const volunteerRouter = Router();
 
@@ -12,6 +13,7 @@ const volunteerController =
 volunteerRouter.post(
   '/volunteers',
   loginRequired,
+  imagesUploader,
   volunteerController.postVolunteer
 );
 
@@ -29,11 +31,13 @@ volunteerRouter.get(
   volunteerController.getRegisterationVolunteer
 );
 
+// 봉사활동 수정 및 삭제 가능 여부 체크(로그인한 사용자가 업로드한 봉사활동인지 체크)
 volunteerRouter.get(
   '/volunteers/check/:volunteerId',
   loginRequired,
   volunteerController.getCheckUser
 );
+
 // 특정 봉사활동 정보 조회
 volunteerRouter.get(
   '/volunteers/:volunteerId',
@@ -47,10 +51,11 @@ volunteerRouter.get('/volunteers', volunteerController.getVolunteer);
 volunteerRouter.patch(
   '/volunteers/:volunteerId',
   loginRequired,
+  imagesUploader,
   volunteerController.patchVolunteer
 );
 
-//
+//등록한 봉사활동 모집상태 수정
 volunteerRouter.patch(
   '/volunteers/registerations/:volunteerId',
   loginRequired,
