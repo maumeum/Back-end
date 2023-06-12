0202736053a4970ee63bf6e9986e2a36fcfd75db
LHI0915 <gowns1998@gmail.com>
2023-06-12 18:07:30 +0900

    Feat : 관리자가 신고접수된 review 조회, 승인, 취소 기능 구현 완료

--- src/routers/reviewRouter.ts
+++ src/routers/reviewRouter.ts
@@ -11,28 +11,28 @@ reviewRouter.get('/review', reviewController.readReview);
 
 reviewRouter.get(
   '/review/detail/:review_id',
-  reviewController.readReviewDetail,
+  reviewController.readReviewDetail
 );
 
 //본인이 작성한 리뷰가 맞는지 확인하는 API
 reviewRouter.get(
   '/review/check/:review_id',
   loginRequired,
-  reviewController.checkUser,
+  reviewController.checkUser
 );
 
 //유저 리뷰 조회 (마이페이지)
 reviewRouter.get(
   '/reviews/users',
   loginRequired,
-  reviewController.readMyReview,
+  reviewController.readMyReview
 );
 //리뷰 생성
 reviewRouter.post(
   '/review',
   loginRequired,
   imagesUploader,
-  reviewController.postReview,
+  reviewController.postReview
 );
 
 //리뷰 수정
@@ -40,29 +40,47 @@ reviewRouter.patch(
   '/review/users/:review_id',
   loginRequired,
   imagesUploader,
-  reviewController.updateReview,
+  reviewController.updateReview
 );
 
+//리뷰 신고
 reviewRouter.patch(
   '/review/users/reports/:review_id',
   loginRequired,
-  reviewController.patchReportReview,
+  reviewController.patchReportReview
 );
 
 //리뷰 삭제
 reviewRouter.delete(
   '/review/users/:review_id',
   loginRequired,
-  reviewController.deleteReview,
+  reviewController.deleteReview
 );
 
 //isParticipate 상태 변경
 reviewRouter.post(
   '/review/users/participation/:volunteer_id',
   loginRequired,
-  reviewController.changeParticipationStatus,
+  reviewController.changeParticipationStatus
 );
 
 reviewRouter.get('/review/search', reviewController.getSearchReviews);
+
+// ===== 관리자 기능 =====
+//adminOnly 추가 예정(테스트 때문에 잠시 빼둠)
+// 신고받은 게시글 전체 조회
+reviewRouter.get('/review/admins/reports', reviewController.getReportedReview);
+
+// 신고받은 게시글 취소(반려)
+reviewRouter.patch(
+  '/review/admins/reports/cancellations/:review_id',
+  reviewController.patchReportedReview
+);
+
+// 신고받은 게시글 승인
+reviewRouter.delete(
+  '/review/admins/reports/applications/:review_id',
+  reviewController.deleteReportedReview
+);
 
 export { reviewRouter };
