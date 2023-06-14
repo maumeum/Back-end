d59e91bdd7de893b0f2aae26a2e431a067eaccfb
Hanna <foureaf@gmail.com>
2023-06-14 11:40:04 +0900

    Feat : 메인에 랜덤 리뷰 뿌려주는 API 추가

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,10 +1,12 @@
-type Constant = {
+type Constants = {
   HASHING_TIMES: number;
   CHANGING_DATE: number;
+  RANDOM_REVIEWS: number;
 };
-const CONSTANTS: Constant = {
+const CONSTANTS: Constants = {
   HASHING_TIMES: 10,
   CHANGING_DATE: 7,
+  RANDOM_REVIEWS: 4,
 };
 
 export { CONSTANTS };
